c14eb6d138838daea31b7d6a878bf05dc077c02f
Thairone-J <thairone.dev@gmail.com>
2024-05-30 20:03:49 -0300

    initial commit

---
+++ heroes.js
@@ -0,0 +1,45 @@
+
+ export const heroes = [
+    {
+      name: 'Pudge',
+      attributes: { intelligence: 16, strength: 25, agility: 11 },
+      skills: {
+        firstSkill: 'Meat Hook',
+        secondSkill: 'Rot',
+        thirdSkill: 'Flesh Heap',
+        fourthSkill: 'Dismember',
+      },
+    },
+    {
+      name: 'Invoker',
+      attributes: { intelligence: 24, strength: 18, agility: 14 },
+      skills: {
+        firstSkill: 'Quas',
+        secondSkill: 'Wex',
+        thirdSkill: 'Exort',
+        fourthSkill: 'Invoke',
+      },
+    },
+    {
+      name: 'Crystal Maiden',
+      attributes: { intelligence: 22, strength: 18, agility: 16 },
+      skills: {
+        firstSkill: 'Crystal Nova',
+        secondSkill: 'Frostbite',
+        thirdSkill: 'Arcane Aura',
+        fourthSkill: 'Freezing Field',
+      },
+    },
+    {
+      name: 'Axe',
+      attributes: { intelligence: 18, strength: 25, agility: 20 },
+      skills: {
+        firstSkill: 'Berserker’s Call',
+        secondSkill: 'Battle Hunger',
+        thirdSkill: 'Counter Helix',
+        fourthSkill: 'Culling Blade',
+      },
+    },
+  ];
+
+
---
+++ script.js
@@ -0,0 +1,76 @@
+import { heroes } from './heroes.js';
+
+const createHeroButton = document.getElementById('createHeroButton');
+const heroSearchContainer = document.getElementById('heroSearchContainer');
+const searchBar = document.getElementById('searchBar');
+
+document.getElementById('createHeroButton').addEventListener('click', function () {
+  createHero();
+});
+
+document.getElementById('searchButton').addEventListener('click', function () {
+  searchHero();
+});
+
+function createHero() {
+  const createHeroSection = document.createElement('div');
+  createHeroSection.id = 'createHeroSection';
+  createHeroSection.className = 'create-hero-section';
+
+  createHeroButton.style.display = 'none';
+
+  heroSearchContainer.insertBefore(createHeroSection, searchBar);
+
+  const heroSkills = document.createElement('div');
+  heroSkills.className = 'hero-skills';
+
+  const individualSkill1 = document.createElement('div');
+  individualSkill1.className = 'individual-skill';
+
+  const individualSkill2 = document.createElement('div');
+  individualSkill2.className = 'individual-skill';
+
+  const individualSkill3 = document.createElement('div');
+  individualSkill3.className = 'individual-skill';
+
+  const individualSkill4 = document.createElement('div');
+  individualSkill4.className = 'individual-skill';
+
+  createHeroSection.appendChild(heroSkills);
+
+  heroSkills.append(individualSkill1, individualSkill2, individualSkill3, individualSkill4);
+
+  const heroAttributes = document.createElement('div');
+  heroAttributes.className = 'hero-attributes';
+
+  createHeroSection.insertBefore(heroAttributes, heroSkills);
+
+  const heroIndividualAttribute1 = document.createElement('div');
+  heroIndividualAttribute1.className = 'hero-indivudal-attribute';
+
+  const heroIndividualAttribute2 = document.createElement('div');
+  heroIndividualAttribute2.className = 'hero-indivudal-attribute';
+
+  const heroIndividualAttribute3 = document.createElement('div');
+  heroIndividualAttribute3.className = 'hero-indivudal-attribute';
+
+  heroAttributes.append(
+    heroIndividualAttribute1,
+    heroIndividualAttribute2,
+    heroIndividualAttribute3
+  );
+
+  const heroPicture = document.createElement('div');
+  heroPicture.className = 'hero-picture';
+
+  createHeroSection.append(heroPicture);
+}
+
+function searchHero() {
+  createHeroButton.style.display = 'none';
+  const searchPreview = document.createElement('div');
+  searchPreview.className = 'search-preview';
+
+  heroSearchContainer.insertBefore(searchPreview, searchBar);
+  searchBar.style.animation = 'slideDown 0.5s ease-out forwards '
+}
