6f9d49f09f0eb5c3fa4ce89ded482f7a44b005c7
Thairone-J <thairone.dev@gmail.com>
2024-06-18 22:04:26 -0300

    Refactor renderHeroCard

--- script.js
+++ script.js
@@ -12,75 +12,8 @@ document.getElementById('searchButton').addEventListener('click', function () {
   searchHero();
 });
 
-
-
 function createHero() {
-  searchBar.style.animation = 'slideDown2 0.7s ease-out forwards';
-  const createHeroSection = document.createElement('div');
-  createHeroSection.id = 'createHeroSection';
-  createHeroSection.className = 'create-hero-section';
-
-  createHeroButton.style.display = 'none';
-
-  heroSearchContainer.insertBefore(createHeroSection, searchBar);
-
-  const heroSkills = document.createElement('div');
-  heroSkills.className = 'hero-skills';
-
-  const individualSkill1 = document.createElement('div');
-  individualSkill1.className = 'individual-skill';
-
-  const individualSkill2 = document.createElement('div');
-  individualSkill2.className = 'individual-skill';
-
-  const individualSkill3 = document.createElement('div');
-  individualSkill3.className = 'individual-skill';
-
-  const individualSkill4 = document.createElement('div');
-  individualSkill4.className = 'individual-skill';
-
-  createHeroSection.appendChild(heroSkills);
-
-  heroSkills.append(individualSkill1, individualSkill2, individualSkill3, individualSkill4);
-
-  const heroAttributes = document.createElement('div');
-  heroAttributes.className = 'hero-attributes';
-
-  createHeroSection.insertBefore(heroAttributes, heroSkills);
-
-  const heroIndividualAttribute1 = document.createElement('div');
-  heroIndividualAttribute1.className = 'hero-indivudal-attribute';
-
-  const heroIndividualAttribute2 = document.createElement('div');
-  heroIndividualAttribute2.className = 'hero-indivudal-attribute';
-
-  const heroIndividualAttribute3 = document.createElement('div');
-  heroIndividualAttribute3.className = 'hero-indivudal-attribute';
-
-  heroAttributes.append(
-    heroIndividualAttribute1,
-    heroIndividualAttribute2,
-    heroIndividualAttribute3
-  );
-
-  const shuffleContainer = document.createElement('div');
-  shuffleContainer.className = 'shuffle-container';
-
-  createHeroSection.append(shuffleContainer);
-
-  const heroShuffleIcon = document.createElement('i');
-  heroShuffleIcon.className = 'hero-shuffle-icon material-icons shuffle';
-  heroShuffleIcon.textContent = 'shuffle';
-  shuffleContainer.append(heroShuffleIcon);
-
-  const heroPictureWraper = document.createElement('div');
-  heroPictureWraper.className = 'hero-picture-wraper';
-  createHeroSection.insertBefore(heroPictureWraper, heroAttributes);
-
-  const heroPicture = document.createElement('div');
-  heroPicture.className = 'hero-picture';
-
-  heroPictureWraper.append(heroPicture);
+  renderHeroCard();
 }
 
 function searchHero() {
@@ -107,4 +40,55 @@ function searchHero() {
   }
 
   searchBar.style.animation = 'slideDown 0.5s ease-out forwards';
+}
+
+function renderHeroCard() {
+  searchBar.style.animation = 'slideDown2 0.7s ease-out forwards';
+
+  const createHeroSection = document.createElement('div');
+  createHeroSection.id = 'createHeroSection';
+  createHeroSection.className = 'create-hero-section';
+
+  createHeroButton.style.display = 'none';
+
+  heroSearchContainer.insertBefore(createHeroSection, searchBar);
+
+  const heroSkills = document.createElement('div');
+  heroSkills.className = 'hero-skills';
+  createHeroSection.appendChild(heroSkills);
+
+  for (let i = 0; i < 4; i++) {
+    const individualSkill = document.createElement('div');
+    individualSkill.className = 'individual-skill';
+    heroSkills.appendChild(individualSkill);
+  }
+
+  const heroAttributes = document.createElement('div');
+  heroAttributes.className = 'hero-attributes';
+  createHeroSection.insertBefore(heroAttributes, heroSkills);
+
+  for (let i = 0; i < 3; i++) {
+    const heroIndividualAttribute = document.createElement('div');
+    heroIndividualAttribute.className = 'hero-indivudal-attribute';
+    heroAttributes.appendChild(heroIndividualAttribute);
+  }
+
+  const shuffleContainer = document.createElement('div');
+  shuffleContainer.className = 'shuffle-container';
+
+  createHeroSection.append(shuffleContainer);
+
+  const heroShuffleIcon = document.createElement('i');
+  heroShuffleIcon.className = 'hero-shuffle-icon material-icons shuffle';
+  heroShuffleIcon.textContent = 'shuffle';
+  shuffleContainer.append(heroShuffleIcon);
+
+  const heroPictureWraper = document.createElement('div');
+  heroPictureWraper.className = 'hero-picture-wraper';
+  createHeroSection.insertBefore(heroPictureWraper, heroAttributes);
+
+  const heroPicture = document.createElement('div');
+  heroPicture.className = 'hero-picture';
+
+  heroPictureWraper.append(heroPicture);
 }
