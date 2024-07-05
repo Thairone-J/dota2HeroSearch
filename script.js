9557aa04dbcb3ecfea97b0d984339642c54a00f1
Thairone-J <thairone.dev@gmail.com>
2024-07-05 00:33:57 -0300

    Minor changes

--- script.js
+++ script.js
@@ -28,29 +28,77 @@ function renderHomePage() {
     });
   }
 }
-function renderPreviewContainer() {
-  const heroPreviewContainer = document.createElement('div');
-  heroPreviewContainer.className = 'hero-preview-container';
-  heroPreviewContainer.id = 'heroPreviewContainer';
-  app.appendChild(heroPreviewContainer);
+//
+//
+// 👇    👇    SIDE CONTROLS    👇    👇
+//
+//
+function renderControlsContainer() {
+  const mainContainer = document.getElementById('mainContainer');
+  const controlsContainer = document.createElement('div');
+  controlsContainer.className = 'controls-container';
+  controlsContainer.id = 'controlsContainer';
+  mainContainer.appendChild(controlsContainer);
+  renderHomeButton();
+  renderCreateHeroButton();
+  renderSaveButtonContainer();
 }
 
-function renderHeroEditorButton() {
+function renderHomeButton() {
   const controlsContainer = document.getElementById('controlsContainer');
-  const heroEditorButton = document.createElement('div');
-  heroEditorButton.className = 'hero-editor-button';
-  heroEditorButton.id = 'heroEditorButton';
+  const homeButtonContainer = document.createElement('div');
+  homeButtonContainer.className = 'control-button';
+  homeButtonContainer.id = 'homeButtonContainer';
+  const homeIcon = document.createElement('i');
+  homeIcon.className = 'material-icons';
+  homeIcon.textContent = 'home';
+  homeButtonContainer.appendChild(homeIcon);
+  homeButtonContainer.addEventListener('click', function () {
+    renderHomePage();
+  });
+  controlsContainer.appendChild(homeButtonContainer);
+}
+
+function renderCreateHeroButton() {
+  const controlsContainer = document.getElementById('controlsContainer');
+  const createHeroButton = document.createElement('div');
+  createHeroButton.className = 'control-button';
+  createHeroButton.id = 'createHeroButton';
   const icon = document.createElement('i');
   icon.className = 'material-icons';
   icon.textContent = 'add';
-  heroEditorButton.appendChild(icon);
+  createHeroButton.appendChild(icon);
 
-  controlsContainer.appendChild(heroEditorButton);
-  heroEditorButton.addEventListener('click', function () {
+  controlsContainer.appendChild(createHeroButton);
+  createHeroButton.addEventListener('click', function () {
     showHeroEditor();
   });
 }
 
+function renderSaveButtonContainer() {
+  const saveButtonContainer = document.createElement('div');
+  saveButtonContainer.className = 'control-button';
+  saveButtonContainer.id = 'saveButtonContainer';
+  const saveIcon = document.createElement('i');
+  saveIcon.className = 'material-icons';
+  saveIcon.textContent = 'save';
+  saveButtonContainer.appendChild(saveIcon);
+  saveButtonContainer.addEventListener('click', () => {
+    saveHero();
+  });
+  const controlsContainer = document.getElementById('controlsContainer');
+  controlsContainer.appendChild(saveButtonContainer);
+}
+
+function deleteButtonContaienr() {
+  const controlsContainer = document.getElementById('controlsContaienr');
+  const deleteButtonContaienr = document.createElement('div');
+}
+//
+//
+// 👇    👇    SEARCH BAR    👇    👇
+//
+//
 function renderSearchContainer() {
   const searchContainer = document.createElement('div');
   searchContainer.className = 'search-container';
@@ -97,106 +145,6 @@ function renderSearchBar() {
   searchBar.appendChild(input);
   searchBar.appendChild(icon);
   searchBarContainer.appendChild(searchBar);
-}
-
-async function loadHeroes() {
-  defaultHeroList = undefined;
-  try {
-    const response = await fetch('http://localhost:3000/heroes');
-    const heroes = await response.json();
-    console.info('Heroes data has been requested');
-    defaultHeroList = heroes.map((hero) => ({
-      id: hero.id,
-      name: hero.name,
-      mainAttr: hero.main_attr,
-      agi: hero.agi,
-      str: hero.str,
-      intel: hero.intel,
-      imageUrl: hero.image_url,
-    }));
-
-    if (defaultHeroList.length > 0) {
-      heroesDataAvaible = true;
-      console.info('Heroes data is avaible: ' + heroesDataAvaible);
-    } else {
-      console.info('Heroes data was requested but not stored.');
-    }
-  } catch (error) {
-    console.error('Erro ao buscar os heróis: ', error);
-  }
-}
-
-function showHero(hero) {
-  const heroCard = document.getElementById('heroCard');
-  if (hero.name !== 'New Hero') {
-    heroCard.style.backgroundImage = `linear-gradient(180deg, rgba(0, 0, 0, 0.163), rgba(0, 0, 0, 0.585)), url(${hero.imageUrl})`;
-  }
-  renderInfoContainer();
-  renderInfos(hero);
-  renderAttributesContainer();
-  renderAttributes(hero);
-  renderMainAttr(hero);
-  tempHero = {
-    id: hero.id,
-    name: hero.name,
-    mainAttr: hero.mainAttr,
-    agi: hero.agi,
-    str: hero.str,
-    intel: hero.intel,
-    imageUrl: hero.imageUrl,
-  };
-}
-
-function renderMainAttr(hero) {
-  clearAttrIcons();
-  const mainAttr = hero.mainAttr;
-  let attrIcon;
-
-  switch (mainAttr) {
-    case 'str':
-      attrIcon = document.getElementById('strengthIcon');
-      break;
-    case 'agi':
-      attrIcon = document.getElementById('agilityIcon');
-      break;
-    case 'intel':
-      attrIcon = document.getElementById('intelligenceIcon');
-      break;
-
-    default:
-      return;
-  }
-
-  attrIcon.classList.add('selected-attribute');
-}
-
-function showHeroEditor() {
-  if (!heroesDataAvaible) {
-    loadHeroes();
-  }
-
-  const emptyHero = { name: 'New Hero', mainAttr: undefined, agi: 0, str: 0, intel: 0 };
-
-  const heroPreviewContainer = document.getElementById('heroPreviewContainer');
-  if (!heroPreviewContainer) {
-    renderHeroCard();
-    showHero(emptyHero);
-  }
-}
-
-function searchHero(queryHero) {
-  if (!heroesDataAvaible) {
-    loadHeroes();
-    return;
-  }
-  let result = defaultHeroList.find((hero) =>
-    hero.name.toLowerCase().startsWith(queryHero.toLowerCase())
-  );
-  if (result) {
-    renderResult(result);
-  } else {
-    clearResult();
-  }
 }
 
 function renderResult(hero) {
@@ -259,6 +207,52 @@ function clearResult() {
   }
 }
 
+function renderPreviewContainer() {
+  const heroPreviewContainer = document.createElement('div');
+  heroPreviewContainer.className = 'hero-preview-container';
+  heroPreviewContainer.id = 'heroPreviewContainer';
+  app.appendChild(heroPreviewContainer);
+}
+//
+//
+// 👇    👇    HERO CARD    👇    👇
+//
+//
+function showHero(hero) {
+  const heroCard = document.getElementById('heroCard');
+  if (hero.name !== 'New Hero') {
+    heroCard.style.backgroundImage = `linear-gradient(180deg, rgba(0, 0, 0, 0.163), rgba(0, 0, 0, 0.585)), url(${hero.imageUrl})`;
+  }
+  renderInfoContainer();
+  renderInfos(hero);
+  renderAttributesContainer();
+  renderAttributes(hero);
+  renderMainAttr(hero);
+  tempHero = {
+    id: hero.id,
+    name: hero.name,
+    mainAttr: hero.mainAttr,
+    agi: hero.agi,
+    str: hero.str,
+    intel: hero.intel,
+    imageUrl: hero.imageUrl,
+  };
+}
+
+function showHeroEditor() {
+  if (!heroesDataAvaible) {
+    loadHeroes();
+  }
+
+  const emptyHero = { name: 'New Hero', mainAttr: undefined, agi: 0, str: 0, intel: 0 };
+
+  const heroPreviewContainer = document.getElementById('heroPreviewContainer');
+  if (!heroPreviewContainer) {
+    renderHeroCard();
+    showHero(emptyHero);
+  }
+}
+
 function renderHeroCard() {
   renderPreviewContainer();
   const heroCard = document.createElement('div');
@@ -302,6 +296,29 @@ function renderInfos(hero) {
       heroName.blur();
     }
   });
+}
+
+function renderMainAttr(hero) {
+  clearAttrIcons();
+  const mainAttr = hero.mainAttr;
+  let attrIcon;
+
+  switch (mainAttr) {
+    case 'str':
+      attrIcon = document.getElementById('strengthIcon');
+      break;
+    case 'agi':
+      attrIcon = document.getElementById('agilityIcon');
+      break;
+    case 'intel':
+      attrIcon = document.getElementById('intelligenceIcon');
+      break;
+
+    default:
+      return;
+  }
+
+  attrIcon.classList.add('selected-attribute');
 }
 
 function renderAttributesContainer() {
@@ -359,88 +376,65 @@ function renderAttributes(hero) {
 function renderShuffleButton() {
   const heroCard = document.getElementById('heroCard');
 
-  const shuffleContainer = document.createElement('div');
-  shuffleContainer.className = 'shuffle-container';
-  shuffleContainer.id = 'shuffleContainer';
+  const shuffleButtonContainer = document.createElement('div');
+  shuffleButtonContainer.className = 'shuffle-button-container';
+  shuffleButtonContainer.id = 'shuffleButtonContainer';
 
-  heroCard.append(shuffleContainer);
+  heroCard.append(shuffleButtonContainer);
 
-  const heroShuffleIcon = document.createElement('i');
-  heroShuffleIcon.className = 'hero-shuffle-icon material-icons shuffle';
-  heroShuffleIcon.textContent = 'shuffle';
-  shuffleContainer.append(heroShuffleIcon);
-  heroShuffleIcon.addEventListener('click', function () {
+  const shuffleIcon = document.createElement('i');
+  shuffleIcon.className = 'material-icons';
+  shuffleIcon.textContent = 'shuffle';
+  shuffleButtonContainer.append(shuffleIcon);
+  shuffleIcon.addEventListener('click', function () {
     shuffleImg();
   });
 }
 
-function renderControlsContainer() {
-  const mainContainer = document.getElementById('mainContainer');
-  const controlsContainer = document.createElement('div');
-  controlsContainer.className = 'controls-container';
-  controlsContainer.id = 'controlsContainer';
-  mainContainer.appendChild(controlsContainer);
-  renderReturnButton();
-  renderHeroEditorButton();
-  renderSaveButtonContainer();
+function searchHero(queryHero) {
+  if (!heroesDataAvaible) {
+    loadHeroes();
+    return;
+  }
+  let result = defaultHeroList.find((hero) =>
+    hero.name.toLowerCase().startsWith(queryHero.toLowerCase())
+  );
+  if (result) {
+    renderResult(result);
+  } else {
+    clearResult();
+  }
 }
+//
+//
+// 👇    👇    CRUD   👇    👇
+//
+//
+async function loadHeroes() {
+  defaultHeroList = undefined;
+  try {
+    const response = await fetch('http://localhost:3000/heroes');
+    const heroes = await response.json();
+    console.info('Heroes data has been requested');
+    defaultHeroList = heroes.map((hero) => ({
+      id: hero.id,
+      name: hero.name,
+      mainAttr: hero.main_attr,
+      agi: hero.agi,
+      str: hero.str,
+      intel: hero.intel,
+      imageUrl: hero.image_url,
+    }));
 
-function renderSaveButtonContainer() {
-  const saveButtonContainer = document.createElement('div');
-  saveButtonContainer.className = 'save-button-container';
-  saveButtonContainer.id = 'saveButtonContainer';
-  const saveIcon = document.createElement('i');
-  saveIcon.className = 'material-icons';
-  saveIcon.textContent = 'save';
-  saveButtonContainer.appendChild(saveIcon);
-  saveButtonContainer.addEventListener('click', () => {
-    saveHero();
-  });
-  const controlsContainer = document.getElementById('controlsContainer');
-  controlsContainer.appendChild(saveButtonContainer);
-}
-
-function renderReturnButton() {
-  const controlsContainer = document.getElementById('controlsContainer');
-  const returnContainer = document.createElement('div');
-  returnContainer.className = 'return-container';
-  returnContainer.id = 'returnContainer';
-  const returnIcon = document.createElement('i');
-  returnIcon.className = 'return-button material-icons';
-  returnIcon.textContent = 'home';
-  returnContainer.appendChild(returnIcon);
-  returnContainer.addEventListener('click', function () {
-    renderHomePage();
-  });
-  controlsContainer.appendChild(returnContainer);
-}
-
-function shuffleImg() {
-  const imgUrl = defaultHeroList[Math.floor(Math.random() * defaultHeroList.length)].imageUrl;
-  tempHero = { ...tempHero, imageUrl: imgUrl };
-  updateImgBg(imgUrl);
-}
-
-function updateImgBg(imgUrl) {
-  const heroCard = document.getElementById('heroCard');
-  heroCard.style.backgroundImage = `linear-gradient(180deg, rgba(0, 0, 0, 0.163), rgba(0, 0, 0, 0.585)), url(${imgUrl})`;
-}
-
-function clearAttrIcons() {
-  const icons = document.querySelectorAll('#strengthIcon, #agilityIcon, #intelligenceIcon');
-  icons.forEach((icon) => {
-    icon.classList.remove('selected-attribute');
-  });
-}
-
-function allowOnlyNumbers(element) {
-  element.addEventListener('input', function (e) {
-    let content = e.target.innerText.replace(/\D/g, '').slice(0, 2);
-    if (content === '' || content === '00') {
-      content = '0';
+    if (defaultHeroList.length > 0) {
+      heroesDataAvaible = true;
+      console.info('Heroes data is avaible: ' + heroesDataAvaible);
+    } else {
+      console.info('Heroes data was requested but not stored.');
     }
-    e.target.innerText = content;
-  });
+  } catch (error) {
+    console.error('Erro ao buscar os heróis: ', error);
+  }
 }
 
 async function saveHero() {
@@ -462,7 +456,10 @@ async function saveHero() {
     const heroesIds = new Set(defaultHeroList.map((item) => item.id));
     const heroExists = idExists(heroesIds, hero.id);
     if (heroExists) {
-      updateHero(hero);
+      await updateHero(hero);
+      renderHomePage();
+
+      loadHeroes();
       return;
     }
 
@@ -518,6 +515,40 @@ async function saveNewHero(hero) {
   }
 }
 
+async function deleteHero(hero) {}
+//
+//
+// 👇    👇    UTILITY   👇    👇
+//
+//
+function shuffleImg() {
+  const imgUrl = defaultHeroList[Math.floor(Math.random() * defaultHeroList.length)].imageUrl;
+  tempHero = { ...tempHero, imageUrl: imgUrl };
+  updateImgBg(imgUrl);
+}
+
+function updateImgBg(imgUrl) {
+  const heroCard = document.getElementById('heroCard');
+  heroCard.style.backgroundImage = `linear-gradient(180deg, rgba(0, 0, 0, 0.163), rgba(0, 0, 0, 0.585)), url(${imgUrl})`;
+}
+
+function clearAttrIcons() {
+  const icons = document.querySelectorAll('#strengthIcon, #agilityIcon, #intelligenceIcon');
+  icons.forEach((icon) => {
+    icon.classList.remove('selected-attribute');
+  });
+}
+
+function allowOnlyNumbers(element) {
+  element.addEventListener('input', function (e) {
+    let content = e.target.innerText.replace(/\D/g, '').slice(0, 2);
+    if (content === '' || content === '00') {
+      content = '0';
+    }
+    e.target.innerText = content;
+  });
+}
+
 function getInputs() {
   const heroPreviewContainer = document.getElementById('heroPreviewContainer');
 
@@ -531,3 +562,5 @@ function getInputs() {
     };
   }
 }
+//
+//
