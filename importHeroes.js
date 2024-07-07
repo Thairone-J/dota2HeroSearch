30dd9be7849d0cbf524d7203103f75315fef1a22
Thairone-J <thairone.dev@gmail.com>
2024-07-07 17:15:41 -0300

    Refac: connection pool

--- importHeroes.js
+++ importHeroes.js
@@ -1,13 +1,17 @@
-import { pool } from 'mysql2';
+import { createPool } from 'mysql2/promise';
 import heroes from './heroes.js';
+import dotenv from 'dotenv';
 
-const pool = createConnection({
+dotenv.config();
+
+const pool = createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
-
-pool.connect();
 
 const queries = [
   'CREATE DATABASE IF NOT EXISTS ' + process.env.DB_NAME,
@@ -30,31 +34,44 @@ const getQueryName = (query) => {
   return match ? match[0].trim() : 'QUERY';
 };
 
-queries.forEach((query) => {
-  const queryName = getQueryName(query);
-  pool.query(query, (err) => {
-    if (err) {
-      console.error(`Error in: ${queryName}: `, err.message);
-    } else {
-      console.log(`${queryName} successfully executed.`);
-    }
-  });
-});
+const runQueries = async () => {
+  try {
+    const connection = await pool.getConnection();
 
-heroes.forEach((hero) => {
-  const query =
-    'INSERT INTO heroes (name, main_attr, agi, str, intel, image_url) VALUES (?, ?, ?, ?, ?, ?)';
-  pool.query(
-    query,
-    [hero.name, hero.main_attr, hero.attrs.agi, hero.attrs.str, hero.attrs.intel, hero.image_url],
-    (err) => {
-      if (err) {
-        console.error('Data insert error: ', err.message);
-      } else {
-        console.log(`${hero.name} imported!`);
+    for (const query of queries) {
+      const queryName = getQueryName(query);
+      try {
+        await connection.query(query);
+        console.log(`${queryName} successfully executed.`);
+      } catch (err) {
+        console.error(`Error in: ${queryName}: `, err.message);
       }
     }
-  );
-});
 
-pool.end();
+    for (const hero of heroes) {
+      const query =
+        'INSERT INTO heroes (name, main_attr, agi, str, intel, image_url) VALUES (?, ?, ?, ?, ?, ?)';
+      try {
+        await connection.query(query, [
+          hero.name,
+          hero.main_attr,
+          hero.attrs.agi,
+          hero.attrs.str,
+          hero.attrs.intel,
+          hero.image_url,
+        ]);
+        console.log(`${hero.name} imported!`);
+      } catch (err) {
+        console.error('Data insert error: ', err.message);
+      }
+    }
+
+    connection.release();
+  } catch (err) {
+    console.error('Connection error: ', err.message);
+  } finally {
+    pool.end();
+  }
+};
+
+runQueries();
