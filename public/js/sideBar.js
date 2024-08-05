d89cdf698bb6f3a521e1d11d68083d19c4bb430f
Thairone-J <thairone.dev@gmail.com>
2024-08-05 02:04:35 -0300

    fix: sidebar module

--- public/js/sideBar.js
+++ public/js/sideBar.js
@@ -1,20 +1,30 @@
 import { state } from '../script.js';
 import { deleteHero, saveHero } from './heroes.js';
-import { renderHomePage } from './userInterface.js';
+import { renderHomePage, showHeroEditor } from './userInterface.js';
 
 const sideBar = {
-  renderControlsContainer: () => {
-    const sideBarContainer = document.getElementById('sideBarContainer');
-    const controlsContainer = document.createElement('div');
-    controlsContainer.className = 'controls-container';
-    controlsContainer.id = 'controlsContainer';
-    sideBarContainer.appendChild(controlsContainer);
-    renderHomeButton();
-    renderCreateHeroButton();
-    renderSaveButton();
-    renderDeleteButton();
+  renderSideBar: () => {
+    const sideBarContainer = document.createElement('div');
+    sideBarContainer.className = 'sidebar-container';
+    sideBarContainer.id = 'sideBarContainer';
+    const mainContainer = document.getElementById('mainContainer');
+    mainContainer.appendChild(sideBarContainer);
+    renderControlsContainer();
+    renderUserSectionContainer();
   },
 };
+
+function renderControlsContainer() {
+  const sideBarContainer = document.getElementById('sideBarContainer');
+  const controlsContainer = document.createElement('div');
+  controlsContainer.className = 'controls-container';
+  controlsContainer.id = 'controlsContainer';
+  sideBarContainer.appendChild(controlsContainer);
+  renderHomeButton();
+  renderCreateHeroButton();
+  renderSaveButton();
+  renderDeleteButton();
+}
 
 function renderHomeButton() {
   const controlsContainer = document.getElementById('controlsContainer');
@@ -123,5 +133,124 @@ function renderDeleteButton() {
   });
   controlsContainer.appendChild(deleteButtonContainer);
 }
+
+
+function renderUserSectionContainer() {
+  const sideBarContainer = document.getElementById('sideBarContainer');
+  const userSectionContainer = document.createElement('div');
+  userSectionContainer.className = 'user-section-container';
+  userSectionContainer.id = 'userSectionContainer';
+  sideBarContainer.appendChild(userSectionContainer);
+  renderHeroesListButton();
+  renderUserPicture();
+}
+
+function renderUserPicture() {
+  const userPictureContainer = document.createElement('div');
+  const userPicture = document.createElement('img');
+  userPicture.className = 'user-picture';
+  userPicture.id = 'userPicture';
+  userPictureContainer.className = 'user-picture-container';
+  userPictureContainer.id = 'userPictureContainer';
+  userPictureContainer.appendChild(userPicture);
+  const userSection = document.getElementById('userSectionContainer');
+  userSection.appendChild(userPictureContainer);
+}
+
+function renderHeroesListButton() {
+  const heroesListButtonContainer = document.createElement('div');
+  heroesListButtonContainer.className = ' heroes-list-button-container';
+  heroesListButtonContainer.id = 'heroesListButtonContainer';
+  const listIcon = document.createElement('img');
+  listIcon.src = './images/icons/menu.svg';
+  listIcon.className = 'list-icon';
+  listIcon.id = 'listIcon';
+
+  const spinIcon = document.createElement('img');
+  spinIcon.src = './images/icons/spin_arrow.svg';
+  spinIcon.className = 'spin-icon';
+  spinIcon.id = 'spinIcon';
+
+  const checkIcon = document.createElement('img');
+  checkIcon.src = './images/icons/check.svg';
+  checkIcon.className = 'check-icon';
+  checkIcon.id = 'checkIcon';
+
+  heroesListButtonContainer.append(listIcon, spinIcon, checkIcon);
+
+  heroesListButtonContainer.addEventListener('click', () => {
+    renderLoginContainer();
+  });
+
+  const userSection = document.getElementById('userSectionContainer');
+  userSection.appendChild(heroesListButtonContainer);
+}
+
+
+function renderLoginContainer() {
+  const loginContainer = document.getElementById('loginContainer');
+
+  if (loginContainer) {
+    loginContainer.remove();
+    return;
+  }
+
+  const token = localStorage.getItem('token');
+
+  const newLoginContainer = document.createElement('div');
+  newLoginContainer.className = 'login-container';
+  newLoginContainer.id = 'loginContainer';
+
+  const mainContainer = document.getElementById('mainContainer');
+  const inputContainer = document.createElement('div');
+  inputContainer.className = 'input-container';
+  const buttonsContainer = document.createElement('div');
+  buttonsContainer.className = 'buttons-container';
+  buttonsContainer.id = 'buttonsContainer';
+
+  mainContainer.append(newLoginContainer);
+  newLoginContainer.append(inputContainer, buttonsContainer);
+
+  if (!token) {
+    const loginInput = document.createElement('input');
+    loginInput.id = 'loginInput';
+    const passwordInput = document.createElement('input');
+    passwordInput.type = 'password';
+    passwordInput.id = 'passwordInput';
+    inputContainer.append(loginInput, passwordInput);
+
+    const loginButton = document.createElement('div');
+    loginButton.className = 'login-button';
+    loginButton.id = 'loginButton';
+    loginButton.textContent = 'Login';
+    loginButton.addEventListener('click', () => {
+      const username = document.getElementById('loginInput').value.toLowerCase();
+      const password = document.getElementById('passwordInput').value;
+
+      login(username, password);
+    });
+
+    const signInButton = document.createElement('div');
+    signInButton.className = 'signin-button';
+    signInButton.id = 'signInButton';
+    signInButton.textContent = 'Sign in';
+    signInButton.addEventListener('click', () => {
+      const username = document.getElementById('loginInput').value.toLowerCase();
+      const password = document.getElementById('passwordInput').value;
+
+      register(username, password);
+    });
+
+    buttonsContainer.append(loginButton, signInButton);
+  } else {
+    const userPicture = document.createElement('div');
+    userPicture.className = 'user-picture';
+    const myHeroesButton = document.createElement('div');
+    myHeroesButton.textContent = 'My Heroes';
+    inputContainer.appendChild(userPicture);
+    buttonsContainer.appendChild(myHeroesButton);
+  }
+}
+
 
 export default sideBar;
