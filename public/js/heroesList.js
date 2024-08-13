e86f59d383fecd7ac8672312a1d1b92809d2dc80
Thairone-J <thairone.dev@gmail.com>
2024-08-13 00:22:02 -0300

    Fix: add event handler to heroes list

--- public/js/heroesList.js
+++ public/js/heroesList.js
@@ -1,8 +1,12 @@
 import { state } from '../script.js';
 import { loadHeroes } from './heroes.js';
 
-export async function renderHeroesList(eventSource) {
+export function renderHeroesList(event) {
   const sideMenuContainer = document.getElementById('sideMenuContainer');
+  const existingElement = document.getElementById('heroesListContainer');
+  if (existingElement) {
+    sideMenuContainer.removeChild(existingElement);
+  }
 
   const heroesListContainer = document.createElement('div');
   heroesListContainer.id = 'heroesListContainer';
@@ -11,12 +15,16 @@ export async function renderHeroesList(eventSource) {
   sideMenuContainer.appendChild(heroesListContainer);
 
   const heroesListGrid = document.createElement('div');
+
   heroesListGrid.id = 'heroesListGrid';
   heroesListGrid.className = 'heroes-list-grid';
 
   heroesListContainer.appendChild(heroesListGrid);
+  handleEventSource(event);
+}
 
-  switch (eventSource) {
+async function handleEventSource(event) {
+  switch (event) {
     case 'changeUserPicture':
       if (!state.heroesDataAvaible) {
         await loadHeroes();
